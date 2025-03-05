0478a468c4a978bacf3365b97bef92748c79fdf3
Rageaquinao <igoreivone@gmail.com>
2025-03-05 17:45:36 +0000

    cartaST nivel aventureiro

--- CartasSuperTrunfo1.c
+++ CartasSuperTrunfo1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
+  //Carta 1
     char Estado1;           
     char Cod1[50];          
     char Nome1[50];         
@@ -10,9 +11,21 @@ int main() {
     int pontost1; 
     float DensidP1;
     float PpC1;
+
+    //Carta 2
+
+    char Estado2;           
+    char Cod2[50];          
+    char Nome2[50];         
+    float Pop2;               
+    float Area2;            
+    float Pib2;             
+    int pontost2; 
+    float DensidP2;
+    float PpC2;
               
 
-    
+    //Cadrastro Carta1
     printf("Digite a letra do estado: \n");
     scanf("%c", &Estado1);  
 
@@ -34,11 +47,37 @@ int main() {
     printf("Digite os pontos do Turísticos: \n");
     scanf("%d", &pontost1);  
 
+    //Cadrastro Carta2
+    printf("Digite a letra do estado: \n");
+    scanf("%c", &Estado2);  
+
+    printf("Digite o código da carta: \n");
+    scanf("%s", Cod2);  
+
+    printf("Digite o nome da cidade s/ espaço: \n");
+    scanf("%s", Nome2);  
+
+    printf("Digite a quantidade da população: \n");
+    scanf("%f", &Pop2);
+
+    printf("Digite a área em km²: \n");
+    scanf("%f", &Area2);
+
+    printf("Digite o PIB da cidade: \n");
+    scanf("%f", &Pib2); 
+
+    printf("Digite os pontos do Turísticos: \n");
+    scanf("%d", &pontost2);  
+
+
+
+
       // Cálculos
       DensidP1 = Pop1 / Area1;  // Densidade Populacional
       PpC1 = Pib1 / Pop1;       // PIB per Capita
   
       // Exibindo os dados
+      printf("Carta 1\n");
       printf("Informações da Cidade:\n");
       printf("Estado: %c\n", Estado1);
       printf("Código da Carta: %s\n", Cod1);
@@ -49,6 +88,23 @@ int main() {
       printf("Pontos Turísticos: %d\n", pontost1);
       printf("Densidade Populacional: %.2f habitantes/km²\n", DensidP1);
       printf("Pib per Capita: %.2f\n", PpC1);  // Exibindo o PIB per Capita com 2 casas decimais
+
+      // Cálculos
+      DensidP2 = Pop2 / Area2;  // Densidade Populacional
+      PpC2 = Pib2 / Pop2;       // PIB per Capita
+  
+      // Exibindo os dados
+      printf("Carta 2\n");
+      printf("Informações da Cidade:\n");
+      printf("Estado: %c\n", Estado2);
+      printf("Código da Carta: %s\n", Cod2);
+      printf("Nome da Cidade: %s\n", Nome2);
+      printf("População: %.2f\n", Pop2);
+      printf("Área: %.2f km²\n", Area2);
+      printf("PIB: %.2f\n", Pib2);
+      printf("Pontos Turísticos: %d\n", pontost2);
+      printf("Densidade Populacional: %.2f habitantes/km²\n", DensidP2);
+      printf("Pib per Capita: %.2f\n", PpC2);  // Exibindo o PIB per Capita com 2 casas decimais
     
     return 0;
 }
